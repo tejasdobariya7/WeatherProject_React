0da5a152fb9f588ddf8899e899ca459fba1705f8
tejas <tejasdobariya8@gmail.com>
2021-07-31 20:51:51 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Weatherdetails from './component/Weatherdetails';
+import './index.css'
+
+
+function App() {
+ 
+    return (
+      <div>
+        <Weatherdetails />
+      </div>
+    )
+  }
+ 
+
+export default App
+
